8994a9dde119953e6ae0797c1b2bf6ba1a10bc22
mrntando-dev <sibonginkosincust@gmail.com>
2025-11-25 12:37:12 +0200

    Create Ais.jsx

---
+++ components/Ais.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Ais = () => {
+  return (
+    <div className="ais-container">
+      {/* AIS component content here */}
+    </div>
+  );
+};
+
+export default Ais;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
